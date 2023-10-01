ff759ed2f6369d30c2da72014860426756df3368
Amarasena B.H.L.M <141402575+LahiruniMalshika@users.noreply.github.com>
2023-10-01 18:31:03 +0530

    Contact Cards

---
+++ App.js
@@ -0,0 +1,66 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./Components/Card.css";
+import me23 from "./Media/me23.jpg";
+
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src={me23}alt="Lahi Malshi"/>;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Lahiruni Malshika</h1>
+      <p>
+        I am an undergraduate student at Faculty of IT University of Moratuwa.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Java" emoji="👶" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+export default App;
